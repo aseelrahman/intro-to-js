5dd028a86d33db53b8d68b40df6519a60e2b96be
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-05-01 10:05:05 -0400

    first commit

---
+++ variable.js
@@ -0,0 +1,19 @@
+/*
+    Variable ==> vary + able
+    5 things to declare a variable
+*/
+// keyword variable_name = value ;
+var alu = 5;
+var price = 38;
+var year = 1995;
+var money = 2005;
+
+var age;
+console.log(age);
+console.log(money);
+
+
+var age = 17;
+age = 35;
+
+console.log(age);
