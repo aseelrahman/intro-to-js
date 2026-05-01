89f3060958c2ba4bd745e93d3430ff9a7c27da7f
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-05-01 10:23:51 -0400

    types of variable in js

---
+++ types.js
@@ -0,0 +1,18 @@
+// string
+var name = "sadaruddin";
+console.log(name);
+var address = "Adar killa bandorban with some katabon"
+var country = "Bangladesh"
+
+// boolean
+var isRich = true;
+console.log(isRich);
+
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
+console.log(typeof country);
+
+// number
+var price = 555;
+console.log(typeof price);
